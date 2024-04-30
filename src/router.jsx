e1dfc57da8fbc8d601e58d693d10137463457496
Aleksey Pecheritsa <fromzeya@gmail.com>
2024-04-30 10:35:50 +0700

    Update router

--- src/router.jsx
+++ src/router.jsx
@@ -1,21 +1,25 @@
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Auth from "./pages/Auth";
 import ToDos from "./pages/ToDos";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route path="/" element={<App />}>
-        <Route index element={<ToDos />} />
-      </Route>
-      <Route path="/login" element={<Auth />} />
-    </>
-  )
-);
+const routerConfig = [
+  {
+    Component: App,
+    path: "/",
+    children: [
+      {
+        Component: ToDos,
+        path: "/",
+      },
+    ],
+  },
+  {
+    Component: Auth,
+    path: "/login",
+  },
+];
+
+const router = createBrowserRouter(routerConfig);
 
 export default router;
